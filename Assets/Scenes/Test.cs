4b45c830477ac36440dd8d556270977f227748b0
Kim-game3 <zweilous387@icloud.com>
2024-09-18 10:03:42 +0900

    joy-con入れた

---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Test : MonoBehaviour
+{
+    void Update()
+    {
+        SwitchControllerAnyKeyDown();
+    }
+
+    private void SwitchControllerAnyKeyDown()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (SwitchController code in Enum.GetValues(typeof(SwitchController)))
+            {
+                if (Input.GetKeyDown((KeyCode)code)) Debug.Log(code);
+            }
+        }
+    }
+}
+
+public enum SwitchController
+{
+    A = 350,
+    B = 352,
+    X = 351,
+    Y = 353,
+    UpArrow = 372,
+    LeftArrow = 370,
+    RightArrow = 373,
+    DownArrow = 371,
+    LStick = 380,
+    RStick = 361,
+    L = 384,
+    R = 364,
+    ZL = 385,
+    ZR = 365,
+    LeftSL = 374,
+    LeftSR = 375,
+    RightSL = 354,
+    RightSR = 355,
+    Minus = 378,
+    Plus = 359,
+    HOME = 362,
+    Capture = 383
+}
